56866b7b2a9f6f0027c738069d6ec8522234093a
SigmaOd <nstch_root_sigma@proton.me>
2024-11-17 12:15:32 +0100

    4: player drawn on screen

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 # from constants import (
 #       SCREEN_WIDTH, 
 #       SCREEN_HEIGHT, 
@@ -16,20 +17,22 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0                              # delta-t
 
 
     while True:
-          for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                      return
+      for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                  return
                 
-
-          screen.fill((0,0,0))
-          pygame.display.flip()
-          
-          # limit Framerate to 60 fps 
-          dt = game_clock.tick(60) / 1000
+                
+      screen.fill((0,0,0))
+      player.draw(screen)
+      pygame.display.flip()
+      
+      # limit Framerate to 60 fps 
+      dt = game_clock.tick(60) / 1000
           
 
 
